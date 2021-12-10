3c626f8fb444dca633842142c2473d285468dc3e
kmcarranza <84938989+kmcarranza@users.noreply.github.com>
2021-12-10 01:08:45 +0000

    Add check for A1 to C3 as input.

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ def clear_board(game_board):
   return game_board
 
 def check_input(user_input):
-  #check if A1 to A9
   if not isnumber(user_input): return False
   user_input= int(user_input)
   if not check_boundary(user_input): return False
@@ -30,13 +29,14 @@ def check_input(user_input):
 
 def isnumber(user_input):
   if not user_input.isnumeric():
-    print("This is not a valid input")
+    print("This is not a valid input.")
     return False
   else:return True 
 
 def check_boundary(user_input):
   if user_input >9 or user_input < 1:
-    print("This number is not 1-9 and out of bounds")
+    print("This is not a valid input.")
+    print("Please enter a position A1 to A9 or enter \"q\" to quit:")
     return False
   else:return True
 
@@ -59,7 +59,7 @@ def current_user(user):
   else: return "0"
 
 def user_input_transform(user_input):
-  conv_user_input = 0
+  conv_user_input = 10
   if user_input == 'A1':
     conv_user_input = 1
   if user_input == "A2":
@@ -82,42 +82,31 @@ def user_input_transform(user_input):
 
 def did_win(game_board,active_user):
     if check_win_row(game_board,active_user): 
-      print('winrow')
       return True
     if check_win_col(game_board,active_user): 
-      print('wincolumn')
       return True
     if check_diagonal(game_board,active_user): 
-      print('win diagonal')
       return True
     else:return False
 
 def check_win_row(game_board,active_user):
     if (game_board[0] == active_user and game_board[1] == active_user and game_board[2] == user): 
-      print(5)
       return True
-    if (game_board[3] == active_user and game_board[4] == active_user and game_board[5] == active_user): 
-      print(6)
+    if (game_board[3] == active_user and game_board[4] == active_user and game_board[5] == active_user):
       return True
     if (game_board[6] == active_user and game_board[7] == active_user and game_board[8] == active_user): 
-      print(7)
       return True
     else:
-      print(8)
       return False
 
 def check_win_col(game_board,active_user):
     if (game_board[0] == active_user and game_board[3] == active_user and game_board[6] == active_user): 
-      print(1)
       return True
     if (game_board[1] == active_user and game_board[4] == active_user and game_board[7] == active_user): 
-      print(2)
       return True
     if (game_board[2] == active_user and game_board[5] == active_user and game_board[8] == active_user): 
-      print(3)
       return True
     else:
-      print(4)
       return False
 
 def check_diagonal(game_board,user):
@@ -132,9 +121,8 @@ while counter < 9:
   if quit(user_input):
     break
   user_input = int(user_input_transform(user_input))
-  # if not check_input(user_input):
-  #   print("Please try again.")
-  #   continue
+  if not check_boundary(user_input):
+    continue
   user_input = int(user_input) - 1
   if istaken(user_input):
     print("Space already taken. Please try again.")
@@ -152,3 +140,8 @@ while counter < 9:
   counter += 1
   if counter == 9:
     print('Tie!')
+    user_input = input ('Do you want to play again ("y" or "n") ? ')
+    if user_input == "y":
+      counter = 0
+      clear_board(game_board)
+    else: break
